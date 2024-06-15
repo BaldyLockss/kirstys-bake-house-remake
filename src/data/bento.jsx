b6d1b809fc8f3fab146e8fd3d9fe09b085ccef52
Anthony Reilly <anthonyreilly0500@gmail.com>
2024-06-15 21:01:16 +0100

    image updates

--- src/data/bento.jsx
+++ src/data/bento.jsx
@@ -12,15 +12,15 @@ export const bento = [
   { src: "/assets/images/cake8.jpg", alt: "50th birthday cake" },
   { src: "/assets/images/cake9.jpg", alt: "chocloate birthday cake" },
   {
-    src: "/assets/images/cake1.jpg",
+    src: "/assets/images/cake10.jpg",
     alt: "terry chocolate orange cake"
   },
-  { src: "/assets/images/cake2.jpg", alt: "harry potter cake" },
-  { src: "/assets/images/cake3.jpg", alt: "crushed oreo cake" },
-  { src: "/assets/images/cake4.jpg", alt: "elegant cake" },
-  { src: "/assets/images/cake5.jpg", alt: "elegant style cake" },
-  { src: "/assets/images/cake6.jpg", alt: "cheesecake jars" },
-  { src: "/assets/images/cake7.jpg", alt: "tray bake brittle bark" },
-  { src: "/assets/images/cake8.jpg", alt: "50th birthday cake" },
-  { src: "/assets/images/cake9.jpg", alt: "chocloate birthday cake" }
+  { src: "/assets/images/cake11.jpg", alt: "harry potter cake" },
+  { src: "/assets/images/cake12.jpg", alt: "crushed oreo cake" },
+  { src: "/assets/images/cake13.jpg", alt: "elegant cake" },
+  { src: "/assets/images/cake14.jpg", alt: "elegant style cake" },
+  { src: "/assets/images/cake15.jpg", alt: "cheesecake jars" },
+  { src: "/assets/images/cake16.jpg", alt: "tray bake brittle bark" },
+  { src: "/assets/images/cake17.jpg", alt: "50th birthday cake" },
+  { src: "/assets/images/cake18.jpg", alt: "chocloate birthday cake" }
 ];
